f5571be4f4431e3cbfbda64bb89b23bc1b4ce3b9
raxsonic <service@raxsonic.com>
2021-10-02 20:45:57 +0100

    Final

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
 // Task: Write a function to:
 // - Generate a gradient using the colors from the pickers and split percentage from the slider.
-// - Use the gradient as the form background.
+let colorOne = document.getElementById("color1")
+let colorTwo = document.getElementById("color2")
+let css = document.getElementById("css")
+let surpriseBtn = document.getElementById("surprise")
+let bkg = document.getElementById("container")
+let output = document.getElementById("gradient")
+let sliderChange = document.getElementById("slider")
+let text = document.getElementById("text")
+let angle = 45
 
-// Stretch goals:
-// - Add more colors.
-// - Add a 'Surprise Me' button which generates random gradients (and updates the inputs).
-// - Check for contrast between the gradient and the text.
+colorOne.addEventListener("input", setGradient)
+colorTwo.addEventListener("input", setGradient)
+sliderChange.addEventListener("input", setGradient)
+
+function setGradient() {
+	let c1 = colorOne.value
+	let c2 = colorTwo.value
+	let s1 = sliderChange.value
+	output.style.background = `linear-gradient(${angle}deg, ${c1}, ${s1}%, ${c2})`
+	css.textContent = output.style.background
+}
+setGradient()
+
+surpriseBtn.addEventListener("click", function () {
+	function getNewColor() {
+		let letters = "0123456789ABCDEF"
+		let color = "#"
+		for (let i = 0; i < 6; i++) {
+			color += letters[Math.floor(Math.random() * 16)]
+		}
+
+		output.style.background = color
+	}
+	getNewColor()
+})
